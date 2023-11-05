1bf82c867498b2dbb3c04e239ba5f28854f82a74
Alessandro Leo <alessandro.leo@iar.com>
2023-11-05 11:08:19 +0000

    BAFA:gallery

---
+++ BAFA/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿function openFullImg(pic) {
+    var fullImgBox = document.getElementById("fullImgBox");
+    var fullImg = document.getElementById("fullImg");
+    fullImgBox.style.display = "flex";
+    fullImg.src = pic;
+}
+
+function closeFullImg() {
+    fullImgBox.style.display = "none";
+}
